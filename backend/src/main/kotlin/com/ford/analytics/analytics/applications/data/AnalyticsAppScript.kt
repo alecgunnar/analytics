1c4a8fff1889b60244c586b16276e437cee6d6b8
Alec Carpenter <acarpe29@ford.com>
2019-04-09 11:31:36 -0400

    Removes the app id variable in the client script; inlines the value in the function call to run analytics.

--- backend/src/main/kotlin/com/ford/analytics/analytics/applications/data/AnalyticsAppScript.kt
+++ backend/src/main/kotlin/com/ford/analytics/analytics/applications/data/AnalyticsAppScript.kt
@@ -5,5 +5,5 @@ import java.util.*
 data class AnalyticsAppScript(
         val id: UUID,
 
-        val script: String = "<script>var a='$id',s=document.createElement('script');s.src='http://localhost:8080/client';s.onload=function(){analytics.run(a);};document.body.appendChild(s);</script>"
+        val script: String = "<script>var s=document.createElement('script');s.src='http://localhost:8080/client';s.onload=function(){analytics.run('$id');};document.body.appendChild(s);</script>"
 )
