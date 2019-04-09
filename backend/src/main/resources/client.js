adcbbe303047cf15573b1f42c66205ac80677020
Alec Carpenter <acarpe29@ford.com>
2019-04-09 09:37:51 -0400

    Adds test to ensure analytics can be collected

--- backend/src/main/resources/client.js
+++ backend/src/main/resources/client.js
@@ -1,5 +1,8 @@
 var analytics = {
     run: function (appId) {
-        console.log('Analytics Client', appId);
+        var request = new XMLHttpRequest();
+
+        request.open('POST', 'http://localhost:8080/apps/' + appId + '/hits');
+        request.send();
     }
 };
--- frontend/tests/e2e/specs/loadingAnApplication.js
+++ frontend/tests/e2e/specs/loadingAnApplication.js
@@ -1,3 +1,5 @@
+const http = require('http')
+
 module.exports = {
     'applications can be created': browser => {
         browser
@@ -23,6 +25,34 @@ module.exports = {
             .pause(1000)
 
             .assert.containsText('[data-qa=app-name]', 'Test App')
+    },
+    'there are no hits immediately after creating the app': browser => {
+        browser.assert.containsText('[data-qa=hits-count]', '0')
+    },
+    'analytics can be collected': browser => {
+        browser.getText('[data-qa=client-script]', (result) => {
+            const server = http.createServer((request, response) => {
+                response.write(`<!DOCTYPE html><html><head><title>e2e</title></head><body><div data-qa="analytics-sample"></div>${result.value}</body></html>`)
+                response.end()
+                server.close()
+            })
+
+            server.listen(8082)
+        })
+
+        browser.url('http://localhost:8082')
+        browser.waitForElementPresent('[data-qa=analytics-sample]', 5000)
+
+        browser
+            .url(process.env.VUE_DEV_SERVER_URL)
+
+            .waitForElementVisible('#app', 5000)
+
+            .click('[data-qa=existing-app] a')
+
+            .pause(1000)
+
+        browser.assert.containsText('[data-qa=hits-count]', '1')
 
         browser.end()
     }
